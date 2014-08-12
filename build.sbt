c63ed0da8c1ff30e42eb8ee3c7291d26e9ee01f7
shimamoto <chibochibo@gmail.com>
2014-08-12 13:10:13 +0900

    WIP: Add sample code.

--- build.sbt
+++ build.sbt
@@ -6,10 +6,11 @@ scalaVersion := "2.11.1"
 
 resolvers ++= Seq(
   "Archiver Managed Spark Repository" at "https://repository.apache.org/content/repositories/orgapachespark-1025/",
+  "sonatype-oss" at "https://oss.sonatype.org/content/repositories/snapshots",
   "conjars.org" at "http://conjars.org/repo"
 )
 
 libraryDependencies ++= Seq(
   "org.apache.spark" % "spark-core_2.10" % "1.1.0",
-  "org.elasticsearch" % "elasticsearch-hadoop" % "2.0.0"
+  "org.elasticsearch" % "elasticsearch-spark_2.10" % "2.1.0.BUILD-SNAPSHOT" changing()
 )
--- src/main/scala/Sample.scala
+++ src/main/scala/Sample.scala
@@ -1,12 +1,27 @@
 import org.apache.spark.{SparkConf, SparkContext}
+//import org.apache.spark.SparkContext._
+
+import org.elasticsearch.spark._
 
 object Sample {
   def main(args: Array[String]) = {
     // TODO want to abstraction for unit test
     val sc = new SparkContext(new SparkConf().setAppName("sample app"))
 
+    /** writing */
+    // creates RDD based on the collection specified
+    val data = sc.makeRDD(Seq(Map("one" -> 1, "two" -> 2, "three" -> 3)))
+    data.saveToEs("spark/docs")
 
+    // For cases where the data in the RDD is already in JSON
+    val data2 = sc.makeRDD(Seq("""{ "reason":"business", "airport":"SFO" }"""))
+    data2.saveJsonToEs("spark/json-trips")
 
+    /** reading */
+    val rdd = sc.esRDD("spark/docs" ,"?t*")
+    val data3 = rdd.map(_.mkString(","))
+
+    println(s"----------- ${data3.collect} -----------")
   }
 
 }
