e1143ec965b61279f01a430335e0f5724c8e5de7
shimamoto <chibochibo@gmail.com>
2014-08-22 00:41:31 +0900

    Implement the flow of the update.

--- src/main/scala/WriteSample.scala
+++ src/main/scala/WriteSample.scala
@@ -4,20 +4,24 @@ import org.elasticsearch.spark._
 import org.elasticsearch.hadoop.cfg.ConfigurationOptions._
 
 object WriteSample extends App {
-  val sc = new SparkContext(new SparkConf().setAppName("write sample")
-    .set(ES_RESOURCE_WRITE, "spark/docs")
-    .set(ES_WRITE_OPERATION, "upsert")
-    .set(ES_MAPPING_ID, "one")
-  )
+  val conf = new SparkConf().setAll(Seq(
+    ES_NODES           -> "localhost",
+    ES_RESOURCE_READ   -> "spark/docs",
+    ES_RESOURCE_WRITE  -> "spark/docs",
+    ES_QUERY           -> """{"query" : { "ids" : { "values" : [ "100", "101" ] } } }""",
+    ES_WRITE_OPERATION -> "upsert",
+    ES_MAPPING_ID      -> "one"
+  ))
 
-  print("------ ")
-  sc.esRDD("spark/docs", """{"query" : { "ids" : { "values" : [ "100" ] } } }""") foreach println
+  new SparkContext(conf).esRDD.flatMap {
+    _.headOption.map { case (_id, body) =>
+      body.asInstanceOf[collection.mutable.Map[String, Any]].toMap + ("two" -> (Math.random * 10).toInt)
+    }
+  }.saveToEs(Map(ES_NODES -> "127.0.0.1"))
 
-  sc.makeRDD(Seq(
-    Map("one" -> 100, "two" -> 222, "three" -> 333)
-  )).saveToEs(Map.empty[String, String])
 
-  print("------ ")
-  sc.esRDD("spark/docs", """{"query" : { "ids" : { "values" : [ "100" ] } } }""") foreach println
+//  new SparkContext(conf).makeRDD(Seq(
+//    Map("one" -> 101, "two" -> 200, "three" -> 300)
+//  )).saveToEs(Map.empty[String, String])
 
 }
