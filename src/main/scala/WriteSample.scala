cd63351743ee47479d78ad7a503c35a1582e4d90
shimamoto <chibochibo@gmail.com>
2014-08-22 22:02:05 +0900

    Change the Docker settings.

--- src/main/scala/WriteSample.scala
+++ src/main/scala/WriteSample.scala
@@ -7,7 +7,7 @@ object WriteSample extends App {
   val conf = new SparkConf().setAll(Seq(
     ES_NODES           -> "localhost",
     ES_RESOURCE_READ   -> "spark/docs",
-    ES_RESOURCE_WRITE  -> "spark/docs",
+    ES_RESOURCE_WRITE  -> "docker/docs",
     ES_QUERY           -> """{"query" : { "ids" : { "values" : [ "100", "101" ] } } }""",
     ES_WRITE_OPERATION -> "upsert",
     ES_MAPPING_ID      -> "one"
@@ -17,7 +17,7 @@ object WriteSample extends App {
     _.headOption.map { case (_id, body) =>
       body.asInstanceOf[collection.mutable.Map[String, Any]].toMap + ("two" -> (Math.random * 10).toInt)
     }
-  }.saveToEs(Map(ES_NODES -> "127.0.0.1"))
+  }.saveToEs(Map(ES_NODES -> "172.17.0.2"))
 
 
 //  new SparkContext(conf).makeRDD(Seq(
