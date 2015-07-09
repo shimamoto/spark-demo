853ab8cdfe8136a121fa6d8cbc14801f81bcf59d
shimamoto <chibochibo@gmail.com>
2015-07-09 21:07:06 +0900

    [WIP] Add Kinesis receiver sample.

--- build.sbt
+++ build.sbt
@@ -2,13 +2,21 @@ name := "spark-demo"
 
 version := "1.0-SNAPSHOT"
 
-scalaVersion := "2.11.5"
+scalaVersion := "2.11.7"
 
 resolvers ++= Seq()
 
 libraryDependencies ++= Seq(
-  "org.apache.spark"  %% "spark-streaming"           % "1.2.1" % "provided",
-  "org.elasticsearch" %  "elasticsearch-spark_2.10"  % "2.1.0.Beta3" exclude("org.apache.spark", "spark-sql_2.10")
+  "org.apache.spark"  %% "spark-streaming"             % "1.4.0" % "provided",
+  "org.apache.spark"  %% "spark-streaming-kinesis-asl" % "1.4.0" exclude("org.apache.spark", "spark-streaming_2.11"),
+  "org.elasticsearch" %% "elasticsearch-spark"         % "2.1.0" exclude("org.apache.spark", "spark-sql_2.11")
 )
 
-assemblySettings
+assemblyMergeStrategy in assembly := {
+  case PathList("META-INF", xs @ _*) =>
+    (xs map {_.toLowerCase}) match {
+      case ("manifest.mf" :: Nil) => MergeStrategy.discard
+      case _ => MergeStrategy.discard
+    }
+  case x => MergeStrategy.first
+}
---
+++ src/main/scala/KinesisSample.scala
@@ -0,0 +1,43 @@
+import com.amazonaws.services.kinesis.clientlibrary.lib.worker.InitialPositionInStream
+import org.apache.spark._
+import org.apache.spark.storage.StorageLevel
+import org.apache.spark.streaming._
+import kinesis.KinesisUtils
+
+object KinesisSample {
+  def main(args: Array[String]): Unit = {
+    val interval = Seconds(30)
+
+    val spark = new StreamingContext(new SparkConf, interval)
+
+    val dstream = spark.union(
+      // TODO shards size
+      (0 until 1).map { _ =>
+        KinesisUtils.createStream(spark,
+          kinesisAppName = "spark-demo-indexer",
+          streamName     = "spark-test",
+          endpointUrl    = "https://kinesis.us-east-1.amazonaws.com",
+          regionName     = "us-east-1",
+          storageLevel   = StorageLevel.MEMORY_AND_DISK,
+          checkpointInterval      = interval,
+          initialPositionInStream = InitialPositionInStream.LATEST)
+      }
+    )
+
+    // print the first 10 wordCounts
+    dstream
+      .flatMap(new String(_) split " ")
+      .map(_ -> 1)
+      .reduceByKey(_ + _)
+      .print()
+
+//    sys.ShutdownHookThread {
+//      println("Gracefully stopping Spark Streaming Application")
+//      spark.stop(true, true)
+//    }
+
+    spark.start
+    spark.awaitTermination
+
+  }
+}
