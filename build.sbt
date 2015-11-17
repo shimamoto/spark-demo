03d75d6e209c0c21e276ee02a67598872538a97b
shimamoto <chibochibo@gmail.com>
2015-11-17 15:49:34 +0900

    Upgrade to Spark 1.5.1.

--- build.sbt
+++ build.sbt
@@ -9,13 +9,13 @@ resolvers ++= Seq(
 )
 
 libraryDependencies ++= Seq(
-  "org.apache.spark"  %% "spark-streaming"             % "1.4.0" % "provided",
-  "org.apache.spark"  %% "spark-streaming-kinesis-asl" % "1.4.0" excludeAll(
+  "org.apache.spark"  %% "spark-streaming"             % "1.5.1" % "provided",
+  "org.apache.spark"  %% "spark-streaming-kinesis-asl" % "1.5.1" excludeAll(
     ExclusionRule("org.apache.spark", "spark-streaming_2.11"),
     ExclusionRule("org.spark-project.spark")
   ),
-  "org.elasticsearch" %% "elasticsearch-spark"         % "2.1.0" exclude("org.apache.spark", "spark-sql_2.11"),
-  "org.optaplanner"    % "optaplanner-core"            % "6.3.0.Final"
+  "org.elasticsearch" %% "elasticsearch-spark"         % "2.1.0" exclude("org.apache.spark", "spark-sql_2.11")
+//  "org.optaplanner"    % "optaplanner-core"            % "6.3.0.Final"
 )
 
 net.virtualvoid.sbt.graph.Plugin.graphSettings
--- src/main/java/planner/JobProcess.java
+++ src/main/java/planner/JobProcess.java
@@ -1,51 +1,51 @@
 package planner;
 
-import org.optaplanner.core.api.domain.entity.PlanningEntity;
-import org.optaplanner.core.api.domain.variable.PlanningVariable;
+//import org.optaplanner.core.api.domain.entity.PlanningEntity;
+//import org.optaplanner.core.api.domain.variable.PlanningVariable;
 
 /**
  * Created by takako.shimamoto on 15/11/05.
  */
-@PlanningEntity
+//@PlanningEntity
 public class JobProcess {
 
-    private int key;
-    private int duration;
-    private CronSchedule explicitSchedule;
-
-    // Planning variables: changes during planning, between score calculations.
-    private CronSchedule schedule;
-
-    public int getKey() {
-        return key;
-    }
-
-    public void setKey(int key) {
-        this.key = key;
-    }
-
-    public int getDuration() {
-        return duration;
-    }
-
-    public void setDuration(int duration) {
-        this.duration = duration;
-    }
-
-    public CronSchedule getExplicitSchedule() {
-        return explicitSchedule;
-    }
-
-    public void setExplicitSchedule(CronSchedule explicitSchedule) {
-        this.explicitSchedule = explicitSchedule;
-    }
-
-    @PlanningVariable(valueRangeProviderRefs = {"scheduleRange"})
-    public CronSchedule getSchedule() {
-        return schedule;
-    }
-
-    public void setSchedule(CronSchedule schedule) {
-        this.schedule = schedule;
-    }
+//    private int key;
+//    private int duration;
+//    private CronSchedule explicitSchedule;
+//
+//    // Planning variables: changes during planning, between score calculations.
+//    private CronSchedule schedule;
+//
+//    public int getKey() {
+//        return key;
+//    }
+//
+//    public void setKey(int key) {
+//        this.key = key;
+//    }
+//
+//    public int getDuration() {
+//        return duration;
+//    }
+//
+//    public void setDuration(int duration) {
+//        this.duration = duration;
+//    }
+//
+//    public CronSchedule getExplicitSchedule() {
+//        return explicitSchedule;
+//    }
+//
+//    public void setExplicitSchedule(CronSchedule explicitSchedule) {
+//        this.explicitSchedule = explicitSchedule;
+//    }
+//
+//    @PlanningVariable(valueRangeProviderRefs = {"scheduleRange"})
+//    public CronSchedule getSchedule() {
+//        return schedule;
+//    }
+//
+//    public void setSchedule(CronSchedule schedule) {
+//        this.schedule = schedule;
+//    }
 }
